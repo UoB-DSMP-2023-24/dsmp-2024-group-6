924b807f59859fd2e5ad51429e4e5b4e5a9af5f0
Leon2322155 <147050449+Leon2322155@users.noreply.github.com>
2024-03-09 01:24:42 +0800

    Add files via upload

---
+++ clean_for_beta.py
@@ -0,0 +1,15 @@
+import pandas as pd
+
+input_txt_path = '/Users/lifushen/Desktop/giana1/output_data.txt'
+output_txt_path = '/Users/lifushen/Desktop/giana1/clean_beta.txt'  # 输出文件的路径和文件名
+
+# 使用pandas读取TXT文件，这里假定使用制表符（'\t'）作为分隔符
+df = pd.read_csv(input_txt_path, sep='\t', usecols=['cdr3', 'v.segm'])
+
+# 仅保留v.segm列以"TRBV"开头的行
+df_filtered = df[df['v.segm'].astype(str).str.startswith('TRBV')]
+
+# 保存处理后的DataFrame到新的TXT文件，同样使用制表符作为字段分隔符
+df_filtered.to_csv(output_txt_path, index=False, sep='\t', header=False)
+
+print(f"处理后的文件已保存到：{output_txt_path}")
