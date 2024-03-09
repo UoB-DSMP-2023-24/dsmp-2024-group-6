c2f7a5e9ceed2e9797fcfb1d05a888755cc594cd
Berlin <Berlinc.sss@gmail.com>
2024-03-09 17:22:04 +0000

    add hamming_distance(GLIPH)

--- tcrdist3_cluster.py
+++ tcrdist3_cluster.py
@@ -22,6 +22,15 @@ res_summary = member_summ(res_df = res, clone_df = tr.clone_df)
 
 res_detailed = pd.concat([res, res_summary], axis = 1)
 
-# html = hierdiff.plot_hclust_props(Z,
-#             title='PA Epitope Example',
-#             res=res_detailed,
+html = hierdiff.plot_hclust_props(Z,
+            title='PA Epitope Example',
+            res=res_detailed,
+            tooltip_cols=['cdr3_a_aa','v_a_gene', 'j_a_gene'],
+            alpha=0.001, colors = ['blue','gray'],
+            alpha_col='pvalue')
+
+"""WRITE HTML TO A DISK"""
+with open('influenza_alpha_chain_tree.html', 'w') as fh:
+    fh.write(html)
+
+print(html)
